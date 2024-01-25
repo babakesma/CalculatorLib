4af015ac9c0a515d63c72e98d0befe0d682ba904
babakesma <babakesma@yahoo.com>
2024-01-25 21:30:01 +0330

    Add fuction for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# This is a test case
+
+# This is an add function
+def add(first_term, second_term):
+    return first_term + second_term
+
+# This is an subtract function
+def sub( first_term, second_term):
+    return first_term - second_term
